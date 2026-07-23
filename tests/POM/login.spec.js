baa162088ea8b44fac5229825fd5509c83bb6e78
mahanteshbalagaon <mahanteshbalagaon01@gmail.com>
2026-07-23 16:09:41 +0530

    test commit

--- tests/POM/login.spec.js
+++ tests/POM/login.spec.js
@@ -18,6 +18,6 @@ import LoginData from "../../testData/loginData.json"
    await page.waitForTimeout(2000)
 
    //Approach2: Combine and call POM methods
-   //await loginpage.loginProcess(un,pwd)
+   await loginpage.loginProcess(un,pwd)
       
 })
